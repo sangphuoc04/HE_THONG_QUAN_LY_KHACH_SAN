e7d04cdf23be2cf71d21d75779d6f7e1a8550a2e
sangPhuoc <Dsang9411@gmail.com>
2025-08-15 09:59:43 +0700

    dev add login

--- se/src/main/java/project/myweb/se/TestConnection.java
+++ se/src/main/java/project/myweb/se/TestConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.apache.coyote.Response;
+// import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ se/src/main/java/project/myweb/se/security/securityConfig.java
@@ -0,0 +1,27 @@
+package project.myweb.se.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(httpForm -> {
+                    httpForm
+                            .loginPage("/login").permitAll();
+                })
+
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/req/signup").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
